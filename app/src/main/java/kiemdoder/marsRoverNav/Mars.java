87dad8b8810fcf1cdef9501db195419e6e5eca4d
Roelf de Kock <roelf.dekock@redstor.com>
2022-01-24 22:12:39 +0200

    added more tests

--- app/src/main/java/kiemdoder/marsRoverNav/Mars.java
+++ app/src/main/java/kiemdoder/marsRoverNav/Mars.java
@@ -52,7 +52,6 @@ public class Mars {
                 }
             }
         }
-        ;
 
         return Optional.of(rovers);
     }
--- app/src/test/java/kiemdoder/marsRoverNav/MarsTest.java
+++ app/src/test/java/kiemdoder/marsRoverNav/MarsTest.java
@@ -23,8 +23,23 @@ class MarsTest {
                 MM
                 0 5 N
                 MLM
+                3 3 W
                 """;
-        assertFalse(Mars.driveRovers(instructions1).isPresent());
+        assertFalse(Mars.driveRovers(instructions2).isPresent());
+
+        String instructions3 = """
+                10 10
+                0 0 H
+                MM
+                """;
+        assertFalse(Mars.driveRovers(instructions3).isPresent());
+
+        String instructions4 = """
+                10
+                0 0 N
+                MM
+                """;
+        assertFalse(Mars.driveRovers(instructions4).isPresent());
     }
 
     @Test
@@ -36,11 +51,23 @@ class MarsTest {
                 3 3 E
                 MMRMMRMRRM
                 """;
-
         Optional<List<Rover>> rovers = Mars.driveRovers(instructions);
         assertTrue(rovers.isPresent());
         rovers.get().forEach(System.out::println);
         assertEquals(rovers.get().get(0).toString(), "1 3 N");
-        assertEquals(rovers.get().get(1).toString(), "5 1 N");
+        assertEquals(rovers.get().get(1).toString(), "5 1 E");
+
+        String instructions2 = """
+                3 3
+                0 0 N
+                MRMLMRMLM
+                3 3 S
+                MMMLM
+                """;
+        Optional<List<Rover>> rovers2 = Mars.driveRovers(instructions2);
+        assertTrue(rovers2.isPresent());
+        rovers2.get().forEach(System.out::println);
+        assertEquals(rovers2.get().get(0).toString(), "2 3 N");
+        assertEquals(rovers2.get().get(1).toString(), "3 0 E");
     }
 }
